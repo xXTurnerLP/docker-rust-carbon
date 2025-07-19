6faa2fd45175f759077bc14fd8085f8eab8a622c
turner <40324267+xXTurnerLP@users.noreply.github.com>
2025-07-19 04:10:24 +0300

    fuck this dogshit, using env vars directly and we ball

--- start_server.sh
+++ start_server.sh
@@ -2,8 +2,31 @@
 
 cd /home/container
 
-MODIFIED_STARTUP=$(eval echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g') # replaces {{var}} to ${var} (so bash can understand it)
+# setup log stuff
+mkdir -p logs/archive
+mv ./logs/*.log logs/archive
 
-printf "Running server with args: '$(eval echo ${MODIFIED_STARTUP})'\n"
+SERVER_STARTED_AT=$(date +%Y-%m-%d_%H:%M:%S)
 
-./RustDedicated -batchmode $(eval echo ${MODIFIED_STARTUP})
+./RustDedicated -logFile - -batchmode \
+	+server.port ${SERVER_PORT} \
+    $( [ -n "${QUERY_PORT}" ] && +server.queryport ${QUERY_PORT} ) \
+    $( [ -n "${SERVER_IDENTITY}" ] && +server.identity "${SERVER_IDENTITY}" ) \
+    +server.gamemode "${GAMEMODE}" \
+    +server.hostname "${HOSTNAME}" \
+    +server.description "${DESCRIPTION}" \
+    +server.url "${SERVER_URL}" \
+	+server.headerimage "${SERVER_IMG}" \
+	+server.logoimage "${SERVER_LOGO}" \
+	+server.maxplayers ${MAX_PLAYERS} \
+	$( [ -z ${MAP_URL} ] && printf "+server.worldsize \"${WORLD_SIZE}\" +server.seed \"${WORLD_SEED}\"" || printf "+server.levelurl \"${MAP_URL}\"" ) \
+	$( [ -n "${SERVER_TAGS}" ] && printf "+server.tags \"${SERVER_TAGS}\"" ) \
+	${ADDITIONAL_ARGS} \
+    | tee -a ./logs/${SERVER_STARTED_AT}.log
+
+# if the "Log ended" string is not in the logs, the docker container was killed forcefully
+if [ $? -eq 0 ]; then
+	printf "### Log ended. Server stopped cleanly @ $(date +%Y-%m-%d\ %H:%M:%S)" | tee -a ./logs/${SERVER_STARTED_AT}.log
+else
+	printf "### Log ended. Server crashed with exit code '$?' @ $(date +%Y-%m-%d_%H:%M:%S)" | tee -a ./logs/${SERVER_STARTED_AT}.log
+fi
